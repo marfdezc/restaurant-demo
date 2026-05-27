d31d0090df190589053eea8092398d0ba4f550f0
marfc <75501666+marfc@users.noreply.github.com>
2026-05-27 17:46:01 +0200

    last change to fix

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ const observer = new IntersectionObserver((entries) => {
   entries.forEach(entry => {
     if (entry.isIntersecting) {
       entry.target.classList.add('visible');
+      observer.unobserve(entry.target);
     }
   });
 }, {
