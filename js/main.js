d7b65ed1c0ef2f250c30f000af753eae1ed23d53
marfc <75501666+marfc@users.noreply.github.com>
2026-05-27 17:18:55 +0200

    ft: final changes Adds gallery, map and a few details to improve UI/UX

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
 console.log("Web cargada correctamente 🚀");
+
+const elements = document.querySelectorAll('.section, .menu-item, .review-card, .gallery-grid img');
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+    }
+  });
+}, {
+  threshold: 0.1
+});
+
+elements.forEach(el => {
+  el.classList.add('fade-in');
+  observer.observe(el);
+});
